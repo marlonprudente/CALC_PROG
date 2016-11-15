033a92f0337ade7488ac4e7befb477d33f29007e
Marlon <marlonmateuspr@gmail.com>
2016-11-15 18:56:21 -0200

    First Commit

--- rom.vhd
+++ rom.vhd
@@ -13,17 +13,17 @@ end entity;
 architecture a_rom of rom is
 	type mem is array (0 to 127) of unsigned(16 downto 0);
 	constant conteudo_rom : mem := (
-		0  => "00000000000000000",--NOP (00000000)
-		1  => "00010100000100000",--ADD A,B ==> A = A + B
-		2  => "00100000000100000",--ADD A,C ==> A = A + C
-		3  => "00100110000100000",--SUB n 	==> A = A - n (11010110)
-		4  => "00110100000100000",--SUB B 	==> A = A - B 
-		5  => "00100000010000010",--LD A,n 	==> A = n load n em A
-		6  => "00100111100000011",--LD C,n 	==> C = n load n em C
-		7  => "00100000010110010",--LD D,E 	==> D = E load C com o dado em E
-		8  => "11111000000001010",--jump	==> Jump to address 20
-		9  => "00000001100000000",--JP (IX)	==> PC = IX The data in IX is loaded into PC. Thus the address of the next instruction fetched is the data in IX.
-		10 => "00001000000001100",--JR e 	==> PC = PC + e Adds a signed constant e to PC.
+		0  => "00000000 000000000",--NOP (00000000)
+		1  => "10000000 000100000",--ADD A,B ==> A = A + B
+		2  => "10000001 000100000",--ADD A,C ==> A = A + C
+		3  => "11010110 000100000",--SUB n 	==> A = A - n (11010110)
+		4  => "10010000 000100000",--SUB B 	==> A = A - B 
+		5  => "00111110 010000010",--LD A,n 	==> A = n load n em A
+		6  => "00001110 100000011",--LD C,n 	==> C = n load n em C
+		7  => "01010011 010110010",--LD D,E 	==> D = E load C com o dado em E
+		8  => "11111111 000001010",--jump	==> Jump to address 20
+		9  => "xxxxxxxx 100000000",--JP (IX)	==> PC = IX The data in IX is loaded into PC. Thus the address of the next instruction fetched is the data in IX.
+		10 => "xxxxxxxx 000001100",--JR e 	==> PC = PC + e Adds a signed constant e to PC.
 		
 		others => (others=>'0')
 	);
